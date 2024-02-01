f3c1dde419ba741a5fcf9b6255ff9242d00e1051
MR'Xoexarno <bayurizkizulfikri@gmail.com>
2024-02-01 21:43:13 +0700

    Update updater.py

--- Uputt/modules/user/updater.py
+++ Uputt/modules/user/updater.py
@@ -175,7 +175,7 @@ async def upstream(client: Client, message: Message):
             repo.__del__()
             return
         await status.edit(
-            "`[HEROKU]: Update Deploy Uputt-Pyrobot Sedang Dalam Proses...`"
+            "`[HEROKU]: Update Deploy Zull-Pyrobot Sedang Dalam Proses...`"
         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
@@ -201,7 +201,7 @@ async def upstream(client: Client, message: Message):
             repo.git.reset("--hard", "FETCH_HEAD")
         await updateme_requirements()
         await status.edit(
-            "`Uputt-Pyrobot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`",
+            "`Zull-Pyrobot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`",
         )
         args = [sys.executable, "-m", "Uputt"]
         execle(sys.executable, *args, environ)
@@ -270,7 +270,7 @@ async def updaterman(client: Client, message: Message):
 add_command_help(
     "update",
     [
-        ["update", "Untuk melihat list pembaruan terbaru dari Uputt-Pyrobot."],
+        ["update", "Untuk melihat list pembaruan terbaru dari Zull-Pyrobot."],
         ["update deploy", "Untuk mengupdate userbot."],
     ],
 )
